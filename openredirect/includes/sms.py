0ec5e027fc6042ad1ca4e6eb9dff67f269df1581
NAREN-P <110370124+NAREN-P@users.noreply.github.com>
2024-07-04 18:52:14 +0530

    Update sms.py

--- openredirect/includes/sms.py
+++ openredirect/includes/sms.py
@@ -1,17 +1,19 @@
 from twilio.rest import Client
 
-# account_sid = 'ACd984f45e2006d275aeb6c0d0cd799bf6'
-# auth_token = '09fd7c1cd5ad6236a7c06145e52c8fbd'
+ account_sid = ''
+auth_token = ''
 
 client = Client(account_sid, auth_token)
 def send_msg(mesg):
     try:
         client = Client(account_sid, auth_token)
         message = client.messages.create(
-            from_='whatsapp:+14155238886',
+            from_='whatsapp:+',
             body=f'Open redirect: {mesg}',
             to='whatsapp:+91enter_number'
         )
         print(f"Message sent successfully: {message.sid} {mesg}")
     except Exception as e:
         print(f"Failed to send message: {e}")
+
+#go and create you twillo account and then use you acc_id and  auth_tocken ,whats app number ,twillo whatsapp number
